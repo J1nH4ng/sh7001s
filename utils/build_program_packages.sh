f5457abe3ef95e97f0a0a074947a227946690b50
J1nH4ng <jinhang@mail.14bytes.com>
2024-08-23 17:12:06 +0800

    :sparkles: feat(build_program_packages.sh): 打包脚本完成

--- utils/build_program_packages.sh
+++ utils/build_program_packages.sh
@@ -33,6 +33,17 @@ function import_output_logs() {
 }
 
 #######################################
+# 环境检查脚本引入
+# Arguments:
+#  None
+#######################################
+function import_env_check() {
+  local script_dir
+  script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+  source "${script_dir}/../share/env_check.sh"
+}
+
+#######################################
 # 当前日期目录创建脚本引入
 # Arguments:
 #  None
@@ -61,11 +72,12 @@ function import_git_clone() {
 #######################################
 function build_java_project() {
   import_make_current_date_dir
-
   make_current_date_dir_main "/usr/local/src/download"
 
+  local module_name
+
+
   echo_info "开始编译 Java 项目"
-  mvn_env_check
 
   echo_info "切换到项目目录，准备打包"
   cd "/usr/local/src/speed-cicd/${project_name}/${package_name}" || {
@@ -73,14 +85,14 @@ function build_java_project() {
     return 1
   }
 
-  local module_name
+
   read -erp "输入需要打包的模块名称：" module_name
   if [ -z "${module_name}" ]; then
     echo_error_basic "模块名称不能为空，脚本将退出"
     return 1
   fi
 
-  ${mvn_bin} clean package -pl "${module_name}" -am -Dmaven.test.skip=true
+  mvn clean package -pl "${module_name}" -am -Dmaven.test.skip=true
 
   echo_info "Java 项目编译成功"
   echo_info "编译后的文件位于：/usr/local/src/speed-cicd/${project_name}/${package_name}/${module_name}/target/ 目录下"
@@ -115,6 +127,91 @@ function build_java_project() {
 }
 
 #######################################
+# node 项目编译
+# Globals:
+#   BASH_SOURCE
+# Arguments:
+#  None
+# Returns:
+#   1 ...
+#######################################
+function build_node_project() {
+  import_make_current_date_dir
+  make_current_date_dir_main "/usr/local/src/download"
+
+  local nodejs_version
+  local build_command
+  local build_method
+  local environment
+  local manual_input
+
+  echo_info "开始编译 node 项目"
+
+  read -erp "输入需要使用的 node 版本：：" nodejs_version
+
+  # 检查版本是否已安装，如果没有则安装
+  if ! nvm ls "${nodejs_version}" | grep -q "N/A"; then
+    echo_info "Node.js 版本 ${nodejs_version} 未安装，正在安装..."
+    nvm install "${nodejs_version}"
+  fi
+
+  nvm use "${nodejs_version}" || {
+    echo_error_basic "切换 node 版本失败，脚本将退出"
+    return 1
+  }
+
+  echo_info "切换到项目目录，准备打包"
+  cd "/usr/local/src/speed-cicd/${project_name}/${package_name}" || {
+    echo_error_basic "切换到项目目录失败，脚本将退出"
+    return 1
+  }
+
+  pnpm install
+
+  read -erp "选择打包方式（1: 预定义命令, 2: 手动输入）：" build_method
+  case "${build_method}" in
+    1)
+      read -erp "输入环境（dev/prod）：" environment
+      case "${environment}" in
+        dev)
+          build_command="npm run build:dev"
+          ;;
+        prod)
+          build_command="npm run build:prod"
+          ;;
+        *)
+          echo_error_basic "未知环境，脚本将退出"
+          return 1
+          ;;
+      esac
+      ;;
+    2)
+      read -erp "输入手动打包命令：" manual_input
+      build_command="${manual_input}"
+      ;;
+    *)
+      echo_error_basic "未知选项，脚本将退出"
+      return 1
+      ;;
+  esac
+
+  echo_indo "开始执行打包命令：${build_command}"
+
+  echo_info "开始压缩文件夹"
+  zip -r "/usr/local/src/download/${current_date}/[${project_name}]-[${package_name}].zip" "/usr/local/src/speed-cicd/${project_name}/${package_name}/dist" || {
+    echo_error_basic "打包失败，脚本将退出"
+    return 1
+  }
+
+  if [ -f "/usr/local/src/download/${current_date}/[${project_name}]-[${package_name}].zip" ]; then
+    echo_info "文件重命名成功"
+    echo_info "具体信息为：/usr/local/src/download/${current_date}/[${project_name}]-[${package_name}].zip"
+  else
+    echo_error_basic "未找到压缩后的 zip 文件，脚本将退出"
+  fi
+}
+
+#######################################
 # main 函数
 # Arguments:
 #  None
@@ -124,9 +221,30 @@ function main() {
     import_banner
     import_output_logs
     import_git_clone
-    mvn_env_check
+    import_env_check
+
+    local choice
+
+    env_check_main
     git_clone_main
-    build_java_project
+
+    echo_info "代码下载完成，准备打包"
+    echo_info "请选择项目类型："
+    echo_info "1. 后端项目"
+    echo_info "2. 前端项目"
+    read -erp "请输入项目类型：" choice
+    case $choice in
+      1)
+        build_java_project
+        ;;
+      2)
+        build_node_project
+        ;;
+      *)
+        echo_error_basic "输入错误，脚本将退出"
+        return 1
+        ;;
+    esac
   fi
 }
 
