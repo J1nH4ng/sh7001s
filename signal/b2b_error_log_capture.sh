095d0dd393518491fa4d5d062924bd500a24e4cd
J1nH4ng <jinhang@mail.14bytes.com>
2024-08-29 17:13:34 +0800

    :fire: refactor(b2b_error_log_capture.sh): 想到一个更好的解决方式，删除旧代码

--- signal/b2b_error_log_capture.sh
+++ signal/b2b_error_log_capture.sh
@@ -3,105 +3,19 @@
 # 块到块错误日志抓取脚本
 # 版权 2024 J1nH4ng<j1nh4ng@icloud.com>
 
-
-#######################################
-# source_db Function：导入数据文件
-# Globals:
-#   BASH_SOURCE
-#   script_dir
-# Arguments:
-#  None
-#######################################
-function source_db() {
-  export script_dir
-  script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-}
-
-
-#######################################
-# check_exclude_words Function：检查是否有需要排除的关键字
-# Arguments:
-#  None
-#######################################
-function check_exclude_words() {
-  local log_line="$1"
-
-  export have_exclude_words
-
-  have_exclude_words=false
-
-  local exclude_words=()
-  local word
-
-  while IFS= read -r word; do
-    if [[ "$word" == 【*】 ]]; then
-      exclude_words+=("${word:1:-1}")
-    fi
-  done < "${script_dir}/../data/error_log/exclude_error_words.txt"
-
-  for word in "${exclude_words[@]}"; do
-    if [[ "$log_line" == *"$word"* ]]; then
-      have_exclude_words=true
-      break
-    fi
-  done
-}
-
-#######################################
-# Find Error Block Function
-# Arguments:
-#  None
-#######################################
-function find_error_block() {
-  local log_file="$1"
-  local in_error_block=false
-
-  local line
-  local tmp_error_block
-
-  tmp_error_block=$(mktemp)
-
-  while IFS= read -r line; do
-    if [[ $line == *"【ERROR】"* ]]; then
-      in_error_block=true
-      check_exclude_words "$line"
-      if [[ $have_exclude_words == false ]]; then
-        echo "$line" >> "${tmp_error_block}"
-      fi
-    elif [[ $line == *"【INFO】"* || $line == *"【WARN】"* || $line == *"【DEBUG】"* ]]; then
-      in_error_block=false
-    elif $in_error_block; then
-      check_exclude_words "$line"
-      if [[ $have_exclude_words == false ]]; then
-        echo "$line" >> "${tmp_error_block}"
-      fi
-    fi
-  done < "$log_file"
-
-  cat "${tmp_error_block}"
-  rm -f "${tmp_error_block}"
-}
-
-
-#######################################
-# test Function：测试函数
-# Arguments:
-#  None
-#######################################
-function test() {
-  source_db
-  find_error_block "${script_dir}/../data/error_log/test_error_logs.log"
-}
-
-
-
-#######################################
-# Main Function
-# Arguments:
-#  None
-#######################################
-function main() {
-  test "$@"
-}
-
-main "$@"
+# 实现思路：
+# 读取日志文件时维护一个 txt 文档，文档中保存如下信息：
+#
+# [ERROR] -1
+# [INFO|WARN] -1
+# drop_flag 0
+# last_line 0
+#
+# 其中 drop_flag 表示是否要输出，last_line 表示监听的日志最新的位置行
+#
+# 基本逻辑：
+# 1. 读取日志信息，将日志开头的标志如【INFO】或【WARN】所在的行位置数更新到 [INFO|WARN] line_number 位置处
+# 2. 当读取到【ERROR】信息块时，进行内部判断，是否在块内含有 exclude_word.txt 文档中所包涵的关键字，如果有则把 drop_flag 置为 1，否则置为 0
+# 3. 当读取到下一个不为【ERROR】信息块时，如果 drop_flag 为 1，则更新对应的 [INFO|WARN] line_number 并把其他的置为 -1
+#    如果 drop_flag 为 0 时，输出从 [INFO|WARN] line_number 到最新的 [INFO|WARN] line_number-1 的信息块
+# 4. 把当前文件的最后一行写入 last_line 位置处
