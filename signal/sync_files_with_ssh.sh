684d2c8879a3b457ba69e6e985fb677992054c2f
J1nH4ng <2838080432@qq.com>
2024-11-06 22:38:56 +0800

    :construction: feat(sync_files): 添加文件同步脚本的设计构思

---
+++ signal/sync_files_with_ssh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ilex
+#
+# @author: J1nH4ng<j1nh4ngh@icloud.com>
+# @date: 2024-11-06
+# @description: 文件同步脚本(SSH 端口可互通时)
+#
+
+# TODO:
+# - [ ] 验证 SSH 联通
+# - [ ] 同步文件的用户
+# - [ ] 校验本机和对端文件一致性
+# - [ ] 文件冲突时解决
