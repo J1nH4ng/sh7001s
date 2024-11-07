695f207928cd43014056ad97ced08cf26fe362dc
J1nH4ng <2838080432@qq.com>
2024-11-07 20:54:52 +0800

    :construction: feat(server_inspection): 添加文件巡检脚本

---
+++ signal/server_inspection.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+##############################################
+# @Author: J1nH4ng                           #
+# @Date: 2024-11-07                          #
+# @Last Modified By: J1nH4ng                 #
+# @Last Modifide Date: 2024-11-07            #
+# @Email: j1nh4ng@icloud.com                 #
+# @Description: 服务器巡检脚本                 #
+##############################################
+
+# 脚本信息
+VERSION="v0.0.1"
+
+# 服务器基本信息
+
+# 需要巡检的内容
+# [TODO) 日期
+report_date=""
+# [TODO) 主机名
+report_hostname=""
+# [TODO) 发行版本
+# [TODO) 内核
+# [TODO) 语言/编码
+# [TODO) 最近启动时间
+# [TODO) 运行时间
+# [TODO) CPU 数量
+# [TODO) CPU 架构
+# [TODO) 内存总量
+# [TODO) 内存剩余
+# [TODO) 内存使用率
+# [TODO) 磁盘总容量
+# [TODO) 磁盘剩余
+# [TODO) 磁盘使用率
+# [TODO) Inode 总量
+# [TODO) Inode 剩余
+# [TODO) Inode 使用量
+# [TODO) IP 地址
+# [TODO) MAC 地址
+# [TODO) 默认网关
+# [TODO) DNS
+# [TODO) 监听端口
+# [TODO) SeLinux
+# [TODO) Firewalld
+# [TODO) 用户
+# [TODO) 空密码用户
+# [TODO) 相同 ID 用户
+# [TODO) 密码过期
+# [TODO) ROOT 用户
+# [TODO) Sudo 授权
+# [TODO) SSH 信任主机
+# [TODO) SSH 协议版本
+# [TODO) 允许 ROOT 远程登录
+# [TODO) 僵尸进程数量
+# [TODO) 自启动服务数量
+# [TODO) 自启动程序数量
+# [TODO) 运行中的任务数
+# [TODO) 计划中的任务数
+# [TODO) 日志服务
+# [TODO) SNMP
+# [TODO) NTP
+# [TODO) JDK 版本
